2047c06b7522717ae9fd12dac887b573eb68b3e3
pavel2593 <shket97@yandex.ru>
2022-06-18 23:37:42 +0300

    ...

--- src/components/ItemSubLink/ItemSubLink.jsx
+++ src/components/ItemSubLink/ItemSubLink.jsx
@@ -11,7 +11,7 @@ const ItemSubLink = ({ isHover, isSelected, isHave, description, taste }) => {
                 <div>Чего сидишь? Порадуй котэ, <a className={[cl.link__text, isHover ? cl.link__text : ''].join(' ')} href="/">купи</a>.</div>
             }
             {!isHave &&
-                <div className={[cl.link, cl.link_disabled].join(' ')}>Печалька, {taste} закончился.</div>
+                <div className={cl.link_disabled}>Печалька, {taste} закончился.</div>
             }
         </div>
     )
